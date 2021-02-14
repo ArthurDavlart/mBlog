98bf59a859e82893df96991ee441d82229694c95
ArthurDavlart <arthur.davletkaliyev@mail.ru>
2021-02-14 20:01:39 +0400

    Добавил проект на spring boot

---
+++ src/main/java/com/example/mBlog/MBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.mBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mBlog/MBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
